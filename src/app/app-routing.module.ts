c351e69d7c8ac53451528698ccef16cecebf4182
ankit <ac2000choudhary@gmail.com>
2021-05-02 17:16:54 +0530

    contributers-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContributorComponent } from './components/contributor/contributor.component';
 import { MemberComponent } from './components/member/member.component';
 
 const routes: Routes = [
   {
     path:"member", component: MemberComponent
+  },
+  {
+    path:"contributor", component: ContributorComponent
   }
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MemberComponent } from './components/member/member.component';
+import { ContributorComponent } from './components/contributor/contributor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MemberComponent
+    MemberComponent,
+    ContributorComponent
   ],
   imports: [
     BrowserModule,
