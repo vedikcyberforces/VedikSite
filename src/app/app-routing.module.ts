a6223e9d7ea310f7bdea498d796a601801f99570
Himanshu Jangid <iamahalofan@gmail.com>
2021-05-03 18:18:47 +0530

    Build Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path:"", component: HomeComponent},
+  {path:"home", component: HomeComponent},
   {path:"login", component: LoginComponent}
 ];
 
